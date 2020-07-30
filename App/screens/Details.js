13c59affe79e380902a597fc5eda6c9f4a15b0d1
WabaScript <rjwahba@gmail.com>
2020-07-29 23:08:54 -0400

    api up

--- App/screens/Details.js
+++ App/screens/Details.js
@@ -3,21 +3,38 @@ import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 
 export default function Details() {
-    const zipcode = 10018
+
     useEffect(() => {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?appid=xxxxx&zip=${zipcode}&units=imperial`)
+        const zipcode = 10018
+        navigator.geolocation.getCurrentPosition(position => {
+            console.log("position", position);
+        })
+        getCurrentWeather({ zipcode });
+        getForecastWeather();
+    }, []);
+
+    const getCurrentWeather = ({ zipcode, coords }) => {
+        let suffix = "";
+        if (zipcode) {
+            suffix = `zip=${zipcode}`;
+        }
+        fetch(`https://api.openweathermap.org/data/2.5/weather?appid=xxxxxx&units=imperial&${suffix}`)
             .then(res => res.json())
             .then(res => console.log('current response', res))
             .catch(err => {
                 console.log('current error', err)
             })
-        fetch(`https://api.openweathermap.org/data/2.5/forecast?appid=xxxxx&zip=${zipcode}&units=imperial`)
+    }
+
+    const getForecastWeather = () => {
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?appid=xxxxxx&units=imperial`)
             .then(res => res.json())
             .then(res => console.log('forecast response', res))
             .catch(err => {
                 console.log('current error', err)
             })
-    }, []);
+
+    }
 
     return (
         null
