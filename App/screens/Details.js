d0858b50db6ded25ad2c795d41bafadf92b03734
WabaScript <rjwahba@gmail.com>
2020-07-29 22:18:12 -0400

    api update

--- App/screens/Details.js
+++ App/screens/Details.js
@@ -3,7 +3,6 @@ import React, { useEffect } from 'react';
 export default function Details() {
     const zipcode = 10018
     useEffect(() => {
-        //api.openweathermap.org/data/2.5/weather?zip=94040,us
         fetch(`https://api.openweathermap.org/data/2.5/weather?appid=xxxxx&zip=${zipcode}&units=imperial`)
             .then(res => res.json())
             .then(res => console.log('current response', res))
