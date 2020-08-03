d40c3015d16c133d082662f30be4c847bfe4ddcb
WabaScript <rjwahba@gmail.com>
2020-08-03 19:04:27 -0400

    navigation setup

--- App/index.js
+++ App/index.js
@@ -1,3 +1,39 @@
+import React from 'react';
 import Details from './screens/Details';
+import Search from './screens/Search';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default Details;
+const Stack = createStackNavigator();
+
+const MainStack = () => (
+    <Stack.Navigator>
+        <Stack.Screen
+            name="Details"
+            component={Details}
+            options={{ title: 'xxxx' }}
+        />
+        <Stack.Screen
+            name="Search"
+            component={Search}
+            options={({ route }) => ({
+                headerTitle: route.params?.title ?? 'title',
+                headerTintColor: 'white',
+                headerStyle: {
+                    backgroundColor: route.params?.color,
+                    shadowColor: 'transparent'
+                }
+            })
+            }
+        />
+    </Stack.Navigator>
+);
+
+
+export default function Index() {
+    return (
+        <NavigationContainer>
+            <MainStack />
+        </NavigationContainer>
+    );
+};
