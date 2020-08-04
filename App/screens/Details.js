51edee0e4d9ef29a95e4315fbe2dfc121807f574
WabaScript <rjwahba@gmail.com>
2020-08-04 12:04:33 -0400

    forecast rendering

--- App/screens/Details.js
+++ App/screens/Details.js
@@ -1,21 +1,52 @@
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, ScrollView } from 'react-native';
+import { ActivityIndicator, ScrollView, Text, View } from 'react-native';
 import * as Location from 'expo-location';
 import { weatherAPI } from '../util/weatherAPI';
 import { Container } from '../components/Container';
 import { WeatherIcon } from '../components/WeatherIcon';
 import { BasicRow } from '../components/List';
-import { H1, H2 } from '../components/Text';
+import { H1, H2, P } from '../components/Text';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { format } from 'date-fns';
+
+const groupForecastByDay = list => {
+    const data = {}
+
+    list.forEach(item => {
+        const [day] = item.dt_txt.split(" ");
+        if (data[day]) {
+            if (data[day].temp_max < item.main.temp_max) {
+                data[day].temp_max = item.main.temp_max;
+            }
+            if (data[day].temp_min > item.main.temp_min) {
+                data[day].temp_min = item.main.temp_min;
+            }
+        } else {
+            data[day] = {
+                temp_min: item.main.temp_min,
+                temp_max: item.main.temp_max
+            };
+        }
+    });
+
+    const formattedList = Object.keys(data).map(key => {
+        return {
+            day: key,
+            ...data[key]
+        }
+    })
+    return formattedList;
+}
 
 export default function Details({ navigation }) {
     const [currentWeather, setCurrentWeather] = useState({});
+    const [forecast, setForecast] = useState([]);
     const [loadingCurrentWeather, setLoadingCurrentWeather] = useState(true);
+    const [loadingForecast, setLoadingForecast] = useState(true);
 
     useEffect(() => {
         const zipcode = 10018
         navigator.geolocation.getCurrentPosition(position => {
-            console.log("position", position);
             getCurrentWeather({ coords: position.coords });
             getForecastWeather({ coords: position.coords });
         })
@@ -24,7 +55,6 @@ export default function Details({ navigation }) {
     const getCurrentWeather = ({ zipcode, coords }) => {
         return weatherAPI('/weather', { zipcode, coords })
             .then(res => {
-                console.log('current response', res)
                 navigation.setParams({ title: res.name })
                 setCurrentWeather(res)
                 setLoadingCurrentWeather(false)
@@ -37,18 +67,21 @@ export default function Details({ navigation }) {
 
     const getForecastWeather = ({ zipcode, coords }) => {
         return weatherAPI('/forecast', { zipcode, coords })
-            .then(res => console.log('forecast response', res))
+            .then(res => {
+                setLoadingForecast(false);
+                setForecast(groupForecastByDay(res.list));
+            })
             .catch(err => {
                 console.log('forecast error', err)
             })
     }
 
+
     return (
-        loadingCurrentWeather ?
+        loadingCurrentWeather || loadingForecast ?
             <Container>
                 <ActivityIndicator color="#fff" size="large" />
             </Container> :
-
 
             <Container>
                 <ScrollView>
@@ -62,8 +95,26 @@ export default function Details({ navigation }) {
                             <H2>{`Low: ${Math.round(currentWeather.main.temp_min)}°`}</H2>
                             <H2>{`High: ${Math.round(currentWeather.main.temp_max)}°`}</H2>
                         </BasicRow>
+
+                        <View style={{ paddingHorizontal: 10, marginTop: 20 }}>
+                            {forecast.map(day => (
+                                <BasicRow
+                                    key={day.day}
+                                    style={{ justifyContent: 'space-between' }}
+                                >
+                                    <P>{format(new Date(day.day), 'cccc')}</P>
+                                    <View style={{ flexDirection: 'row' }}>
+                                        <P style={{ fontWeight: '700', marginRight: 10 }}>
+                                            {Math.round(day.temp_max)}
+                                        </P>
+                                        <P>{Math.round(day.temp_min)}</P>
+                                    </View>
+                                </BasicRow>
+                            ))}
+                        </View>
                     </SafeAreaView>
                 </ScrollView>
             </Container>
     )
+
 }
